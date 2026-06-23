54d2e0b69053177d078d4c022e5ff6e4d1a4f0d9
xpronk <ducnhungpham335@gmail.com>
2026-06-23 08:02:36 +0800

    Add perplexity.js

---
+++ script/perplexity.js
@@ -0,0 +1,34 @@
+async function request(method, params) {
+  return new Promise((resolve, reject) => {
+    const httpMethod = $httpClient[method.toLowerCase()];
+    httpMethod(params, (error, response, data) => {
+      resolve({ error, response, data });
+    });
+  });
+}
+
+async function main() {
+  const { error, response, data } = await request(
+    "GET",
+    "https://www.perplexity.ai"
+  );
+
+  if (error) {
+    $done({ content: "Network Error", backgroundColor: "" });
+    return;
+  }
+
+  if (response.status >= 200 && response.status < 400) {
+    $done({ content: "Available", backgroundColor: "#88A788" });
+  } else {
+    $done({ content: "Blocked", backgroundColor: "" });
+  }
+}
+
+(async () => {
+  main()
+    .then((_) => {})
+    .catch((error) => {
+      $done({});
+    });
+})();
