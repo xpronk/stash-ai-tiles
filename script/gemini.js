fdbb75d2a041fa69131c605ea0cd3c1cbacbc86c
xpronk <ducnhungpham335@gmail.com>
2026-06-23 08:02:34 +0800

    Add gemini.js

---
+++ script/gemini.js
@@ -0,0 +1,36 @@
+async function request(method, params) {
+  return new Promise((resolve, reject) => {
+    const httpMethod = $httpClient[method.toLowerCase()];
+    httpMethod(params, (error, response, data) => {
+      resolve({ error, response, data });
+    });
+  });
+}
+
+async function main() {
+  const { error, response, data } = await request(
+    "GET",
+    "https://gemini.google.com"
+  );
+
+  if (error) {
+    $done({ content: "Network Error", backgroundColor: "" });
+    return;
+  }
+
+  if (response.status >= 200 && response.status < 400) {
+    $done({ content: "Available", backgroundColor: "#88A788" });
+  } else if (data && data.toLowerCase().includes("not available")) {
+    $done({ content: "Unavailable", backgroundColor: "" });
+  } else {
+    $done({ content: "Blocked", backgroundColor: "" });
+  }
+}
+
+(async () => {
+  main()
+    .then((_) => {})
+    .catch((error) => {
+      $done({});
+    });
+})();
